18647e1ffacc854ec90a0e6ee11dc58d07fab8ff
Diana <dianabistrik@gamil.com>
2025-12-17 14:59:49 +0200

    .

--- Telhai.CS.CsharpCourse.DesignPatterns/Program.cs
+++ Telhai.CS.CsharpCourse.DesignPatterns/Program.cs
@@ -7,6 +7,16 @@ namespace Telhai.CS.CsharpCourse.DesignPatterns
     {
         static void Main(string[] args)
         {
+            string userName = Environment.UserName;
+            string machineName = Environment.MachineName;
+            string osVersion = Environment.OSVersion.ToString();
+
+
+            Console.WriteLine($"Current User: {userName}");
+            Console.WriteLine($"Machine Name: {machineName}");
+            Console.WriteLine($"Operating System: {osVersion}");
+            Console.WriteLine("-----");
+
             //Client client = new Client();
 
             //// This way the client code can support the simple leaf
