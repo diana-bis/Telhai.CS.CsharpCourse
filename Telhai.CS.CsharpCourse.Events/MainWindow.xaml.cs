eb49eff37187b0e044d7ef123358ee069879a71b
Diana <dianabistrik@gamil.com>
2025-12-17 17:16:15 +0200

    new event proj

---
+++ Telhai.CS.CsharpCourse.Events/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Telhai.CS.CsharpCourse.Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //--01 define the deligate for the event
+        public event EventHandler<Color> ColorChanged;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+
+            this.Title += Environment.ProcessId.ToString();
+            this.Title += Environment.MachineName.ToString();
+            this.Title += Environment.UserName.ToString();
+            this.Title += Guid.NewGuid().ToString();
+
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            GetColors();
+        }
+
+        public void GetColors()
+        {
+            //1) Use reflection to get all colors from class: Colors
+            PropertyInfo[] colorsProps =
+                typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static);
+            //object obj = colorsProps[0].GetValue(null);
+            //if (obj is Color color)
+            //{
+                
+            //}
+
+            // Now do LINQ
+            //2) Create a list of anonymous objects with Name and Brush properties
+            var colors = colorsProps.Select(p => new
+            {
+                //Name = p.Name + "(" + p?.GetValue(null).ToString() + ")",
+                Name = p.Name,
+                Brush = new SolidColorBrush((Color)p.GetValue(null))
+            })
+                .OrderBy(c=> c.Name)
+                .ToList();
+            //3) Bind the list to the ListBox
+            this.lstColors.ItemsSource = colors;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">the object invoker</param>
+        /// <param name="e">data object related to the event</param>
+        private void lstColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ListBox lb)
+            {
+                if(lb.SelectedItem is not null)
+                {
+                    // Get the selected color
+                    dynamic selectedColor = lb.SelectedItem;
+
+                    if (selectedColor.Brush is SolidColorBrush brushColor) 
+                    { 
+                        Color colorSelected = brushColor.Color;
+                        //--02 Raise the custom event
+                        if (ColorChanged != null)
+                        {
+                            ColorChanged.Invoke(this, colorSelected);
+                        }
+                    }
+
+                    //Color color = ((SolidColorBrush)selectedColor.Brush).Color;
+                    
+                }
+            }
+        }
+
+        public void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            SubWindow subwindow = new SubWindow();
+            //--03 Subscribe to the custom event
+            this.ColorChanged += subwindow.MainWindow_ColorCahnged;
+            subwindow.Show();
+        }
+    }
+}
---
+++ Telhai.CS.CsharpCourse.Events/SubWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Telhai.CS.CsharpCourse.Events
+{
+    /// <summary>
+    /// Interaction logic for SubWindow.xaml
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        /// <summary>
+        /// 04 get the event from MainWindow
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void MainWindow_ColorCahnged(object? sender, Color color)
+        {
+            SubWindow subwindow = new SubWindow();
+            this.Background = new SolidColorBrush(color);
+        }
+    }
+}
